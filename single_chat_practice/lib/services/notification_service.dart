29aa52aaae642a3cf367c5083dafce2d92329355
홍순원 <hongsun-won@hongsun-won-ui-MacBookPro.local>
2022-01-14 14:45:14 +0900

    foreground notification not yet

--- single_chat_practice/lib/services/notification_service.dart
+++ single_chat_practice/lib/services/notification_service.dart
@@ -16,12 +16,25 @@ class NotificationService extends GetxService {
       streamChatService.client.value.addDevice(token, PushProvider.firebase);
     });
 
+    foregroundNotification();
+
     super.onInit();
   }
 
   void backgroundNotification(Event event) async {
     final client = streamChatService.client.value;
+    _showNotification(client, event);
+  }
 
+  void foregroundNotification() async {
+    streamChatService.client.value
+        .on(EventType.messageNew, EventType.notificationMessageNew)
+        .listen((event) async {
+      _showNotification(streamChatService.client.value, event);
+    });
+  }
+
+  void _showNotification(StreamChatClient client, Event event) async {
     if (![EventType.messageNew, EventType.notificationMessageNew]
             .contains(event.type) ||
         event.user!.id == client.state.currentUser!.id) {
@@ -32,25 +45,38 @@ class NotificationService extends GetxService {
     }
 
     final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-    const initializationSettingsAndroid = AndroidInitializationSettings(
-      'launch_background',
-    );
-
-    const initializationSettingsIOS = IOSInitializationSettings();
     const initializationSettings = InitializationSettings(
-      android: initializationSettingsAndroid,
-      iOS: initializationSettingsIOS,
+      android: AndroidInitializationSettings('launch_background'),
+      iOS: IOSInitializationSettings(),
     );
 
-    await flutterLocalNotificationsPlugin.initialize(initializationSettings);
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: (channelCid) async {
+        if (channelCid != null) {
+          var channel = client.state.channels[channelCid];
+
+          if (channel == null) {
+            final splits = channelCid.split(':');
+            final type = splits[0];
+            final id = splits[1];
+            channel = client.channel(type, id: id);
+            await channel.watch();
+          }
+
+          lgr.Logger().d("Noti Click!");
+        }
+      },
+    );
+
     await flutterLocalNotificationsPlugin.show(
       event.message!.id.hashCode,
       event.message!.user!.name,
       event.message!.text,
       const NotificationDetails(
         android: AndroidNotificationDetails(
-          'message channel',
-          'Message channel',
+          'message channel id',
+          'Message Channel Name',
           channelDescription: 'Channel used for showing messages',
           priority: Priority.high,
           importance: Importance.high,
@@ -58,72 +84,5 @@ class NotificationService extends GetxService {
         iOS: IOSNotificationDetails(),
       ),
     );
-  }
-
-  void foregroundNotification() {
-    final client = streamChatService.client.value;
-
-    streamChatService.client.value
-        .on(EventType.messageNew, EventType.notificationMessageNew)
-        .listen((event) async {
-      if (![EventType.messageNew, EventType.notificationMessageNew]
-              .contains(event.type) ||
-          event.user!.id == client.state.currentUser!.id) {
-        return;
-      }
-      if (event.message == null) {
-        return;
-      }
-
-      final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-      const initializationSettingsAndroid = AndroidInitializationSettings(
-        'launch_background',
-      );
-
-      const initializationSettingsIOS = IOSInitializationSettings();
-      const initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid,
-        iOS: initializationSettingsIOS,
-      );
-
-      await flutterLocalNotificationsPlugin.initialize(
-        initializationSettings,
-        onSelectNotification: (channelCid) async {
-          if (channelCid != null) {
-            var channel = client.state.channels[channelCid];
-
-            if (channel == null) {
-              final splits = channelCid.split(':');
-              final type = splits[0];
-              final id = splits[1];
-              channel = client.channel(
-                type,
-                id: id,
-              );
-
-              await channel.watch();
-            }
-
-            lgr.Logger().d("Noti Click!");
-          }
-        },
-      );
-
-      await flutterLocalNotificationsPlugin.show(
-        event.message!.id.hashCode,
-        event.message!.user!.name,
-        event.message!.text,
-        const NotificationDetails(
-          android: AndroidNotificationDetails(
-            'message channel id',
-            'Message Channel Name',
-            channelDescription: 'Channel used for showing messages',
-            priority: Priority.high,
-            importance: Importance.high,
-          ),
-          iOS: IOSNotificationDetails(),
-        ),
-      );
-    });
   }
 }
